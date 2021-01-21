b85bef0a0ade1d7b7237b36dca60380f84e327d7
Alexander Julian <ajulian@hawk.iit.edu>
2021-01-21 10:06:29 -0600

    Added support for nanopore and illumina sequencing

--- Sequencing_Calculator.py
+++ Sequencing_Calculator.py
@@ -1,7 +1,7 @@
 import sys,argparse
 
 usage = """
-Version 0.0.1
+Version 0.1.1
 Date 1/20/21
 Author A. Julian
 
@@ -12,16 +12,19 @@ or how many cells are required to obtain a specific amount of DNA for sequencing
 -c | --content\t [optional] The GC content of the genome (default: .41)
 -l | --length\t The length of the the organisms genome
 -w | --ploidy\t The number of copies of chromosomes (default: 1, haploid)
--p | --pellet\t The mass of the DNA pellet [micrograms]
--m | --mass\t The desired amount of mass [micrograms]
--s | --sequencer\t [optional] The sequencer that is to be used {nanopore, illumina, pacbio}
+-p | --pellet\t The mass of the DNA pellet [grams]
+-m | --mass\t The desired amount of mass [grams]
+-s | --sequencer\t [optional] The sequencer that is to be used {nanopore, illumina}
 
 Examples
 
+Determine the mass of a DNA pellet 
 Sequencing_Calculator.py -n 10*4 -l 2.5*10**6 -c .46
 
+Determine the required number of cells for successfull sequencing
 Sequencing_Calculator.py -l 2.5*10**6 -s nanopore
 
+Determine the cell count from a DNA pellet
 Sequencing_Calculator.py -l 2.5*10**6 -m 1.5
 """
 
@@ -46,6 +49,7 @@ G_mass = 347.2
 AT_mass = A_mass + T_mass
 CG_mass = C_mass + G_mass
 Avos_num = 6.022*(10**23)
+sequencing_weight = {"nanopore":10*(10**-6),"illumina":500*(10**-9)}
 
 if args.content:
     avg_mass = ((1-args.content)*AT_mass + args.content*CG_mass)
@@ -57,15 +61,22 @@ if not args.length:
     sys.exit()
 if not args.mass and not args.pellet:
     print("Calculating the mass yeild from cell count...")
-    print((args.count*args.length*(10**9))/Avos_num)
+    mass = (args.count*args.length*(10**9))/Avos_num
 elif not args.count:
     if args.mass:
         print("Calculating the number of cells required for sequencing...")
-        print(args.mass/(args.length*avg_mass*(10**9)))
+        count = args.mass/(args.length*avg_mass*(10**9))
+    elif args.sequencer:
+        try:
+            mass = sequencing_weight[args.sequencer]
+            print("Calculating the number of cells required for sequencing...")
+            count = mass/(args.length*avg_mass*(10**9)) 
+        except:
+            print("Improper sequencer submitted. Please select from the following list: {nanopore, illumina}")
+            sys.exit()
     else:
         print("Calculating the number of cells cultivated...")
-        print(args.pellet)
-        print(args.mass/(args.length*avg_mass*(10**9)))
+        count = args.mass/(args.length*avg_mass*(10**9))
 else:
     print("Either the number of cells or the mass obtained/required is needed for calculations...")
     sys.exit()
